aba28870c7f5f07635e29bc178c45857a54721c9
Naveen Manikanta Kodigudla <naveenmanikanta111@gmail.com>
2016-12-21 21:51:49 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,40 @@
+<?php
+$message="";
+if(count($_POST)>0) {
+$conn = mysql_connect("localhost","root","");
+mysql_select_db("phppot_examples",$conn);
+$result = mysql_query("SELECT * FROM users WHERE userName='" . $_POST["userName"] . "' and password = '". $_POST["password"]."'");
+$count  = mysql_num_rows($result);
+if($count==0) {
+$message = "Invalid Username or Password!";
+} else {
+$message = "You are successfully authenticated!";
+}
+}
+?>
+<html>
+<head>
+<title>User Login</title>
+<link rel="stylesheet" type="text/css" href="styles.css" />
+</head>
+<body>
+<form name="frmUser" method="post" action="">
+<div class="message"><?php if($message!="") { echo $message; } ?></div>
+<table border="0" cellpadding="10" cellspacing="1" width="500" align="center">
+<tr class="tableheader">
+<td align="center" colspan="2">Enter Login Details</td>
+</tr>
+<tr class="tablerow">
+<td align="right">Username</td>
+<td><input type="text" name="userName"></td>
+</tr>
+<tr class="tablerow">
+<td align="right">Password</td>
+<td><input type="password" name="password"></td>
+</tr>
+<tr class="tableheader">
+<td align="center" colspan="2"><input type="submit" name="submit" value="Submit"></td>
+</tr>
+</table>
+</form>
+</body></html>
